3331336c314535c21e0148374706a8627ade0e07
tcnksm <nsd22843@gmail.com>
2015-11-04 23:50:33 +0900

    Add Elliptic Curve Digital Signagure Algorithm (ECDSA)

--- signature/ecdsa/main.go
+++ signature/ecdsa/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"fmt"
+)
 
+func main() {
+	// 224-255 bits length key is same strenght with 2048 bits RSA
+	priv, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		fmt.Printf("Err: %s\n", err)
+		return
+	}
+
+	hashed := []byte("This is message.")
+	r, s, err := ecdsa.Sign(rand.Reader, priv, hashed)
+	if err != nil {
+		fmt.Printf("Err: %s\n", err)
+		return
+	}
+
+	if ecdsa.Verify(&priv.PublicKey, hashed, r, s) {
+		fmt.Printf("Verified!\n")
+	}
 }
